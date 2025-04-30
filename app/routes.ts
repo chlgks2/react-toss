7b2dc25bf5337058d21c7723fcfdb0a6cb42c1b5
chlgks2 <duddngud3@gmail.com>
2025-04-30 18:23:19 +0900

    hero_01

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,23 @@
-import { index, prefix, route, type RouteConfig } from '@react-router/dev/routes';
+// import { index, layout, prefix, route, type RouteConfig } from '@react-router/dev/routes';
+
+// // TODO: 경로 정의 (https://reactrouter.com/start/framework/routing)
+// export default [
+//   // * Pages
+//   layout('routes/layouts/default-layout.tsx', [index('routes/pages/home.tsx')]),
+
+//   // * APIs
+//   ...prefix('api', [
+//     route('theme', 'routes/apis/theme.ts'),
+//     route('language', 'routes/apis/language.ts'),
+//   ]),
+// ] satisfies RouteConfig;
+
+import { index, layout, prefix, route, type RouteConfig } from '@react-router/dev/routes';
 
 // TODO: 경로 정의 (https://reactrouter.com/start/framework/routing)
 export default [
   // * Pages
-  index('routes/pages/home.tsx'),
+  layout('routes/layouts/default-layout.tsx', [index('routes/pages/home/routes.tsx')]),
 
   // * APIs
   ...prefix('api', [
